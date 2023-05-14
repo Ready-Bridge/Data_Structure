04af420bcd4c51451d087f765d5e03a61c2ec148
HanJunGyo <127172335+Ready-Bridge@users.noreply.github.com>
2023-05-14 18:01:43 +0900

    C를 사용하여 이진트리 구현
    구조체를 사용하여 이진트리를 구현하고 
    구현한 이진트리의 전위, 중위, 후위 순회를 각각 구현,
    스택을 사용한 중위 순회와 (원형)큐를 사용한 레벨순회도 구현

---
+++ chapter8/TreeNode.c
@@ -0,0 +1,157 @@
+/*
+    15
+ 4    20
+1    16 25
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 10
+
+typedef int element;
+
+typedef struct TreeNode 
+{
+  element key;
+  struct TreeNode *left, *right;
+}TreeNode;
+
+typedef struct{
+  TreeNode* Stack[N];
+  int top;
+}StackType;
+
+typedef struct {
+  TreeNode* Queue[N];
+  int front, rear;
+}QueueType;
+
+void initQueue(QueueType *Q) {
+  Q -> front = Q -> rear = 0;
+}
+
+int isEmpty(QueueType *Q) {
+  return Q -> front == Q -> rear;
+}
+
+void enqueue(QueueType* Q, TreeNode * e) {
+  Q -> rear = (Q -> rear + 1) % N;
+  Q -> Queue[Q -> rear] = e;
+}
+
+TreeNode* dequeue(QueueType *Q) {
+  Q -> front = (Q -> front + 1) % N;
+  return Q -> Queue[Q -> front];
+}
+
+void levelOrder(TreeNode *root) {
+  QueueType Q;
+  initQueue(&Q);
+
+  enqueue(&Q, root);
+  while(!isEmpty(&Q)) {
+    root = dequeue(&Q);
+    printf("[%d] ", root -> key);
+
+    if(root -> left) // NULl 이 아니면 실행
+      enqueue(&Q, root -> left);
+    if(root -> right)
+      enqueue(&Q, root -> right);
+  }
+}
+
+void initStack(StackType *S) {
+  S -> top = -1;
+}
+
+void push(StackType *S, TreeNode* e) {
+  if(S -> top < N - 1)
+    S -> Stack[++S->top] = e;
+}
+
+TreeNode* pop(StackType *S) {
+  TreeNode *e = NULL; // 스택이 비어있다면 처음에 NULL로 초기화 해놨읜까 NULL로 리턴하겠다는 뜻
+
+  if(S -> top >= 0)
+    e = S -> Stack[S -> top--];
+  
+  return e;
+}
+
+void iterOrder(TreeNode *root) {
+  StackType S;
+  initStack(&S);
+
+  while(1) {
+    for(; root != NULL; root = root -> left)
+      push(&S, root);
+    
+    root = pop(&S);
+
+    if(root == NULL)
+      break;
+    
+    printf("[%d] ", root -> key);
+
+    root = root -> right;
+  }
+}
+
+TreeNode* makeNode(element key, TreeNode* left, TreeNode* right)
+{
+  TreeNode *node = (TreeNode *)malloc(sizeof(TreeNode));
+  node -> key = key;
+  node -> left = left;
+  node -> right = right;
+
+  return node;
+}
+
+void preOrder(TreeNode* root) 
+{
+  if(root != NULL) 
+  {
+    printf("[%d] ", root -> key);
+    preOrder(root -> left);
+    preOrder(root -> right);
+  }
+}
+
+void inOrder(TreeNode* root) 
+{
+  if(root != NULL) 
+  {
+    inOrder(root -> left);
+    printf("[%d] ", root -> key);
+    inOrder(root -> right);
+  }
+}
+
+void postOrder(TreeNode* root) 
+{
+  if(root != NULL) 
+  {
+    postOrder(root -> left);
+    postOrder(root -> right);
+    printf("[%d] ", root -> key);
+  }
+}
+
+int main() {
+  TreeNode *N4 = makeNode(1, NULL, NULL); //공집합이 들어가야함
+  TreeNode *N6 = makeNode(16, NULL, NULL); //공집합이 들어가야함
+  TreeNode *N7 = makeNode(25, NULL, NULL); //공집합이 들어가야함
+  TreeNode *N2 = makeNode(4, N4, NULL);
+  TreeNode *N3 = makeNode(20, N6, N7);
+  TreeNode *N1 = makeNode(15, N2, N3);
+  
+  printf("preOrder : "); preOrder(N1); printf("\n"); // 전위 순회
+  printf("inOrder : "); inOrder(N1); printf("\n"); // 중위 순회
+  printf("postOrder : "); postOrder(N1); printf("\n"); // 후위 순회
+
+  printf("iterOrder : "); iterOrder(N1); printf("\n"); //스택을 사용한 중위 순회
+  printf("levelOrder : "); levelOrder(N1);//(원형)큐를 사용한 레벨순회
+  return 0;
+}
